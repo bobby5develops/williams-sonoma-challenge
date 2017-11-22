38e6351c1b60f4aada3ed41cd608a44fc43db433
bobby5develops <bobby5develops@gmail.com>
2017-11-22 10:27:17 -0500

    fixed modal null elements by checking for onload event before invoking modal

--- app/js/modalModule.js
+++ app/js/modalModule.js
@@ -6,30 +6,32 @@ var modalModule = (function (event) {
         this.modalFn = modalFn();
     }*/
 
+
     function modalFn() {
+
         // Get the modal
-        var modal = document.getElementById('modalId');
+        var modal = document.querySelector('#modalId');
 
         // Get the button that opens the modal
         var btnEl = document.querySelector('.add_to_cart');
-
+        console.log(btnEl)
         // Get the <span> element that closes the modal
-        var spanEl = document.getElementsByClassName("close")[0];
-
+        //var spanEl = document.querySelector('.close')[0];
+        //console.log(spanEl);
         // When the user clicks on the button, open the modal
         btnEl.onclick = function() {
-            return modal.style.display = "block";
+            return modal.style.display = 'block';
         };
 
         // When the user clicks on <span> (x), close the modal
-        spanEl.onclick = function() {
-            return modal.style.display = "none";
-        };
+        /*spanEl.onclick = function() {
+            return modal.style.display = 'none';
+        };*/
 
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
             if (event.target == modal) {
-                modal.style.display = "none";
+                modal.style.display = 'none';
             }
         };
         return modal;
@@ -38,8 +40,7 @@ var modalModule = (function (event) {
 
     return {
         /*init: init*/
-        modal: modalFn
+        modalFn: modalFn
     }
 })(event);
 
-modalModule.modal();
